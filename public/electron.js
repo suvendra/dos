130b13b58adc162087e7a2e7eebae56c0c0e18b5
Suvendra <suvendra.ee@gmail.com>
2018-09-05 17:38:43 +0530

    Release commit

--- public/electron.js
+++ public/electron.js
@@ -1,6 +1,8 @@
 const electron = require("electron");
+const { autoUpdater } = require("electron-updater")
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
+const GH_TOKEN = "64e4e9d70945a197282802fef13ef5df73412f15";
 
 const path = require("path");
 const isDev = require("electron-is-dev");
@@ -34,4 +36,25 @@ app.on("activate", () => {
   if (mainWindow === null) {
     createWindow();
   }
+});
+autoUpdater.on('checking-for-update', () => {
+  sendStatusToWindow('Checking for update...');
+})
+autoUpdater.on('update-available', (info) => {
+  sendStatusToWindow('Update available.');
+})
+autoUpdater.on('update-not-available', (info) => {
+  sendStatusToWindow('Update not available.');
+})
+autoUpdater.on('error', (err) => {
+  sendStatusToWindow('Error in auto-updater. ' + err);
+})
+autoUpdater.on('download-progress', (progressObj) => {
+  let log_message = "Download speed: " + progressObj.bytesPerSecond;
+  log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
+  log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
+  sendStatusToWindow(log_message);
+})
+autoUpdater.on('update-downloaded', (info) => {
+  sendStatusToWindow('Update downloaded');
 });
